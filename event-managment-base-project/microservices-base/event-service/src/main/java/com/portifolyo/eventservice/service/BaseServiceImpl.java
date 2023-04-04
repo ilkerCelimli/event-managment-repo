0d5d3a0e828b647b723425f72ab2994a58439947
ilker <ilker-7@hotmail.com>
2023-04-04 13:59:43 +0300

    added notfound exception and baseService

---
+++ event-managment-base-project/microservices-base/event-service/src/main/java/com/portifolyo/eventservice/service/BaseServiceImpl.java
@@ -0,0 +1,42 @@
+package com.portifolyo.eventservice.service;
+
+import com.portifolyo.eventservice.entity.BaseEntity;
+import com.portifolyo.eventservice.exceptions.NotFoundException;
+import com.portifolyo.eventservice.repository.BaseRepository;
+import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+import java.util.Optional;
+
+@Log4j2
+public class BaseServiceImpl<T extends BaseEntity> {
+
+    protected final BaseRepository<T> baseRepository;
+
+    public BaseServiceImpl(BaseRepository<T> baseRepository) {
+        this.baseRepository = baseRepository;
+    }
+
+    protected T save(T entity) {
+        T saved = this.baseRepository.save(entity);
+        log.info("saved id {}",saved.getId());
+        return saved;
+    }
+
+    protected T findById(String id) {
+        Optional<T> entity = this.baseRepository.findById(id);
+        entity.orElseThrow(() -> new NotFoundException(id));
+        return entity.get();
+    }
+
+    protected List<T> findAll() {
+        return this.baseRepository.findAll();
+    }
+
+    protected void delete(String id) {
+        T entity = findById(id);
+        entity.setIsDeleted(true);
+        this.baseRepository.save(entity);
+    }
+
+}
