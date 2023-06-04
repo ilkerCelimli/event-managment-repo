00ad4ffd363a4ed3c9b15ff85d7188809f17644a
ilkerCelimli <ilkerCelimli@outlook.com>
2023-06-04 13:06:01 +0300

    config server added

---
+++ event-managment-base-project/microservices-base/gateway-server/src/main/java/com/portifolyo/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,32 @@
+package com.portifolyo.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.net.URI;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/user/**")
+						.uri("http://localhost:9011/"))
+
+				.route(r -> r.path("/event/**")
+						.uri("http://localhost:9001/"))
+				.build();
+
+	}
+
+}
