fc2a754c0c8dfb9c3229343cb7e585e2382f0b1d
ilkerCelimli <ilkerCelimli@outlook.com>
2023-07-18 15:24:15 +0300

    refactoring all

--- event-managment-base-project/microservices-base/event-service/src/main/java/com/portifolyo/eventservice/service/impl/OrganizatorServiceImpl.java
+++ event-managment-base-project/microservices-base/event-service/src/main/java/com/portifolyo/eventservice/service/impl/OrganizatorServiceImpl.java
@@ -1,6 +1,8 @@
 package com.portifolyo.eventservice.service.impl;
 
 import com.portifolyo.eventservice.entity.Organizator;
+import com.portifolyo.eventservice.exceptions.GenericException;
+import com.portifolyo.eventservice.exceptions.NotFoundException;
 import com.portifolyo.eventservice.repository.OrganizatorRepository;
 import com.portifolyo.eventservice.repository.projections.OrganizatorInfo;
 import com.portifolyo.eventservice.service.BaseServiceImpl;
@@ -8,7 +10,10 @@ import com.portifolyo.eventservice.service.EventAndOrganizatorManyToManyService;
 import com.portifolyo.eventservice.service.OrganizatorService;
 import com.portifolyo.eventservice.util.mapper.OrganizatorRequestMapper;
 import org.portifolyo.requests.eventservice.OrganizatorRequest;
+import org.portifolyo.utils.UpdateHelper;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.InvocationTargetException;
 
 @Service
 public class OrganizatorServiceImpl extends BaseServiceImpl<Organizator> implements OrganizatorService {
@@ -29,21 +34,52 @@ public class OrganizatorServiceImpl extends BaseServiceImpl<Organizator> impleme
 
     @Override
     public OrganizatorInfo findOrganizatorByEmail(String email) {
-        return null;
+        Organizator organizator = this.organizatorRepository.findOrganizatorByEmailEquals(email)
+                .orElseThrow(() -> new NotFoundException(String.format("Not found %s",email)));
+        return OrganizatorInfo.builder()
+                .id(organizator.getId())
+                .tcNo(organizator.getEmail())
+                .name(organizator.getName())
+                .phoneNumber(organizator.getPhoneNumber())
+                .surName(organizator.getSurname())
+                .build();
     }
 
     @Override
     public Organizator findOrganizatorByEmailEntity(String email) {
-        return null;
+        return this.organizatorRepository.findOrganizatorByEmailEquals(email)
+                .orElseThrow(() -> new NotFoundException(String.format("Not found %s",email)));
     }
 
     @Override
     public OrganizatorInfo updateOrganizator(OrganizatorRequest or, String id) {
-        return null;
+        try{
+            UpdateHelper<OrganizatorRequest,Organizator> updateHelper = new UpdateHelper<>();
+            Organizator organizator = this.organizatorRepository.findById(id).orElseThrow(() -> new NotFoundException(
+                    String.format("%s id not found",id)
+            ));
+            Organizator updated = updateHelper.updateHelper(or,organizator);
+            if(updated == null) throw new GenericException("update problem",500);
+            return OrganizatorInfo.builder()
+                    .id(updated.getId())
+                    .tcNo(updated.getTcNo())
+                    .name(updated.getName())
+                    .surName(updated.getSurname())
+                    .mail(updated.getEmail())
+                    .phoneNumber(updated.getPhoneNumber()).build();
+
+        }
+        catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException exception){
+            throw new GenericException("Update problems",500);
+        }
+
     }
 
     @Override
     public void deleteOrganizator(String id) {
-            //TODO not implemented
+        Organizator organizator = this.organizatorRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Not found %s",id)));
+        organizator.setIsDeleted(true);
+        save(organizator);
     }
 }
