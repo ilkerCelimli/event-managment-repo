b0dc0fac33129ce70dc0b125621e0c631decc79c
ilkerCelimli <ilkerCelimli@outlook.com>
2023-07-28 13:54:22 +0300

    securityConfig refactoring

---
+++ event-managment-base-project/microservices-base/event-service/src/main/java/com/portifolyo/eventservice/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.portifolyo.eventservice.config;
+
+import com.portifolyo.eventservice.security.AccessDeniedErrorPoint;
+import com.portifolyo.eventservice.security.CustomAuthenticationEntryPoint;
+import com.portifolyo.eventservice.security.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtFilter jwtFilter, CustomAuthenticationEntryPoint authenticationEntryPoint, AccessDeniedErrorPoint accessDeniedErrorPoint) throws Exception {
+        httpSecurity.csrf().disable();
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity.authorizeHttpRequests(i -> {
+           i.requestMatchers("/ticket/**").hasRole("USER");
+           i.requestMatchers(HttpMethod.PUT,"/organizator/").hasRole("USER");
+           i.requestMatchers(HttpMethod.POST,"/organizator/").hasRole("USER");
+           i.requestMatchers("/organizator/findorganizarevents","/organizator/findorganizatorbyemail").permitAll();
+           i.requestMatchers("/event/").hasRole("USER");
+           i.requestMatchers(HttpMethod.GET,"/event/").permitAll();
+        });
+        httpSecurity.exceptionHandling(i-> {
+            i.accessDeniedHandler(accessDeniedErrorPoint);
+            i.authenticationEntryPoint(authenticationEntryPoint);
+        });
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+}
